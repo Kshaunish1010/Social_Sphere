97a18804159ee703d3566725e790132e0c0fbb6c
Kshaunish1010 <kkfd614@gmail.com>
2023-10-14 23:21:24 +0530

    changes in navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="left">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span>lamasocial</span>
+          <span>Social Sphere</span>
         </Link>
         <HomeOutlinedIcon />
         {darkMode ? (
